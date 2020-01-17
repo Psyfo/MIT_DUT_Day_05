08a209374c32996281dc43066a92a96f76c06234
Psyfo <21422869@dut4life.ac.za>
2020-01-17 12:13:52 +0200

    First commit

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 12:02:08 2020
+
+@author: Admin
+"""
+
+name = "Sipho"
+
+print("My name is " + name + " (Concatenation)")
+print("My name is",name,"(Comma notation)")
+print("My name is {} (Positional arguments)".format(name))
+print(f"My name is {name} (F strings)")
