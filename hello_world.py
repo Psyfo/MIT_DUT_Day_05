39f99c9836c6c5df65f8f98c6893303755258b49
Psyfo <21422869@dut4life.ac.za>
2020-01-20 11:33:05 +0200

    Dicitionary prac 1

--- hello_world.py
+++ hello_world.py
@@ -5,9 +5,22 @@ Created on Fri Jan 17 12:02:08 2020
 @author: Admin
 """
 
-name = "Sipho"
+def get_name():
+    name = input("What is your name? ")
 
-print("My name is " + name + " (Concatenation)")
-print("My name is",name,"(Comma notation)")
-print("My name is {} (Positional arguments)".format(name))
-print(f"My name is {name} (F strings)")
+    print("My name is " + name + " (Concatenation)")
+    print("My name is",name,"(Comma notation)")
+    print("My name is {} (Positional arguments)".format(name))
+    print(f"My name is {name} (F strings)")
+    
+def dict_prac_01():
+    grades = {'Ana':'B', 'John':'A+', 'Denise':'A', 'Katy':'A'}
+    name = input("Whose grade do you want? ")
+    if name in grades.keys():
+        print(f'{name} earned a grade of: {grades[name]}')
+    else:
+        print('That name cannot be found')
+
+
+dict_prac_01()
+    
